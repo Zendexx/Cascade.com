2cf5e5dd62ee6718e693ce73c675ed2e9e5b379e
Zendexx <124657409+Zendexx@users.noreply.github.com>
2023-06-06 19:37:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,102 +1,98 @@
 // Grab elements
 const selectElement = (selector) => {
-    const element = document.querySelector(selector);
-    if(element) return element;
-    throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
+  return document.querySelector(selector);
 };
 
 //Nav styles on scroll
-const scrollHeader = () =>{
-    const navbarElement = selectElement('#header');
-    if(this.scrollY >= 15) {
-        navbarElement.classList.add('activated');
-    } else {
-        navbarElement.classList.remove('activated');
-    }
-}
+const scrollHeader = () => {
+  const navbarElement = selectElement('#header');
+  if (window.scrollY >= 15) {
+      navbarElement.classList.add('activated');
+  } else {
+      navbarElement.classList.remove('activated');
+  }
+};
 
 window.addEventListener('scroll', scrollHeader);
 
 // Open menu & search pop-up
 const menuToggleIcon = selectElement('#menu-toggle-icon');
 
-const toggleMenu = () =>{
-    const mobileMenu = selectElement('#menu');
-    mobileMenu.classList.toggle('activated');
-    menuToggleIcon.classList.toggle('activated');
-}
+const toggleMenu = () => {
+  const mobileMenu = selectElement('#menu');
+  mobileMenu.classList.toggle('activated');
+  menuToggleIcon.classList.toggle('activated');
+};
 
 menuToggleIcon.addEventListener('click', toggleMenu);
-
-
-
 
 // Switch theme/add to local storage
 const body = document.body;
 const themeToggleBtn = selectElement('#theme-toggle-btn');
 const currentTheme = localStorage.getItem('currentTheme');
 
-
 // Check to see if there is a theme preference in local Storage, if so add the light theme to the body
 if (currentTheme) {
-    body.classList.add('light-theme');
+  body.classList.add('light-theme');
 }
 
 themeToggleBtn.addEventListener('click', function () {
-    // Add light theme on click
-    body.classList.toggle('light-theme');
+  // Add light theme on click
+  body.classList.toggle('light-theme');
 
-    // If the body has the class of light theme then add it to local Storage, if not remove it
-    if (body.classList.contains('light-theme')) {
-        localStorage.setItem('currentTheme', 'themeActive');
-    } else {
-        localStorage.removeItem('currentTheme');
-    }
+  // If the body has the class of light theme then add it to local Storage, if not remove it
+  if (body.classList.contains('light-theme')) {
+      localStorage.setItem('currentTheme', 'themeActive');
+  } else {
+      localStorage.removeItem('currentTheme');
+  }
 });
 
+// Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
 
-let slideIndex = 0;
-const slides = document.getElementsByClassName("slide");
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-
-function showSlides() {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-showSlides();
+function showSlides(n) {
+  const slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {
+      slideIndex = 1;
+  }
+  if (n < 1) {
+      slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
 
+// Automated scrolling
 function automateScrolling() {
-    const slides = document.querySelectorAll('.product-slide');
-    const interval = 2000; // Set the interval time in milliseconds (e.g., 2000ms = 2 seconds)
-    let currentSlide = 0;
-  
-    function showSlide(index) {
+  const slides = document.querySelectorAll('.product-slide');
+  const interval = 2000; // Set the interval time in milliseconds (e.g., 2000ms = 2 seconds)
+  let currentSlide = 0;
+
+  function showSlide(index) {
       slides.forEach((slide) => {
-        slide.style.display = 'none';
+          slide.style.display = 'none';
       });
-  
+
       slides[index].style.display = 'block';
-    }
-  
-    function nextSlide() {
+  }
+
+  function nextSlide() {
       currentSlide++;
       if (currentSlide >= slides.length) {
-        currentSlide = 0;
+          currentSlide = 0;
       }
       showSlide(currentSlide);
-    }
-  
-    setInterval(nextSlide, interval);
   }
-  
-  document.addEventListener('DOMContentLoaded', automateScrolling);
-  
+
+  setInterval(nextSlide, interval);
+}
+
+document.addEventListener('DOMContentLoaded', automateScrolling);
